a8ec71192f1db6351652b4d42d1a68922c76bb9b
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2025-05-13 13:15:28 -0500

    TokenType

---
+++ Lexing/Tokens/TokenType.cs
@@ -0,0 +1,73 @@
+﻿namespace Ectoplasm.Lexing.Tokens;
+
+public enum TokenType
+{
+    // Not a real token
+    Undefined,
+    
+    // Keywords
+    Do,
+    End,
+    If,
+    Then,
+    Elseif,
+    Else,
+    For,
+    In,
+    While,
+    Repeat,
+    Until,
+    Goto,
+    Function,
+    Local,
+    Break,
+    Return,
+    
+    // Logical keywords
+    Nil,
+    False,
+    True,
+    And,
+    Or,
+    Not,
+    
+    // Delimiter symbols
+    Statement, // Semicolon ;
+    Separator, // Comma ,
+    Varargs, // Ellipses ...
+    LabelSep, // Double colon ::
+    OpenExp, // Open parenthesis (
+    CloseExp, // Close parenthesis )
+    OpenIndex, // Open bracket [
+    CloseIndex, // Close bracket ]
+    OpenTable, // Open curly bracket {
+    CloseTable, // Close curly bracket }
+    
+    // Operator symbols
+    Add, // '+'
+    Sub, // '-' (Also unary minus)
+    Mul, // '*'
+    Div, // '/'
+    IntDiv, // '//' (Integer/floor division)
+    Exp, // '^' (Power/exponentiation)
+    Mod, // '%'
+    BitwiseAnd, // '&'
+    BitwiseXor, // '~' (Also unary bitwise not)
+    BitwiseOr, // '|'
+    ShiftRight, // '>>'
+    ShiftLeft, // '<<'
+    Concat, // '..'
+    LessThan, // '<'
+    LessThanOrEq, // '<='
+    GreaterThan, // '>'
+    GreaterThanOrEq, // '>='
+    EqualTo, // '=='
+    NotEqualTo, // '~='
+    IndexName, // '.'
+    IndexMethod, // ':'
+    
+    // Value tokens
+    Name, // Identifiers
+    Numeral, // Numbers, both float and integer
+    String // String literals
+}
