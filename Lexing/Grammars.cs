673af2cf8e70378f619bd798b49e4b5b9756116b
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2025-05-14 13:52:49 -0500

    Number parsing
    Needs testing

--- Lexing/Grammars.cs
+++ Lexing/Grammars.cs
@@ -99,13 +99,18 @@ public static partial class Grammars
     public static partial Regex MatchName { get; }
     
     /// <summary>
-    /// Matches both integer and floating point decimal and hex numbers, all valid in Lua. The first capture will
-    /// contain the hex identifier (0x or 0X), the next three will be the significand, fraction, and exponent for a
-    /// hex number, and the next three after that will be significand, fraction, and exponent for a decimal number. The
-    /// significand will be only digits, the fraction will have a leading period, and the exponent will have a leading
-    /// e/E/p/P and possibly sign.
+    /// Matches both integer and floating point decimal and hex numbers, all valid in Lua. Captures:
+    /// 0: Hex identifier 0x or 0X if present. If empty, number is not hexadecimal.
+    /// 1 through 3: Empty if not hex.
+    /// 1: Hex significand.
+    /// 2: Hex fraction, if present, with leading period.
+    /// 3: Decimal exponent (power of 2), if present, with leading p or P and possibly a sign following it.
+    /// 4 through 6: Empty if hex.
+    /// 4: Decimal significand.
+    /// 5: Decimal fraction, if present, with leading period.
+    /// 6: Decimal exponent (power of 10), if present, with leading e or E and possibly a sign following it.
     /// </summary>
-    [GeneratedRegex(@"(0[xX])([\da-fA-F]+)(\.[\da-fA-F]+)?([pP][+-]?[\da-fA-F]+)?|(\d+)(\.\d+)?([eE][+-]?\d+)?")]
+    [GeneratedRegex(@"(0[xX])([\da-fA-F]+)(\.[\da-fA-F]+)?([pP][+-]?\d+)?|(\d+)(\.\d+)?([eE][+-]?\d+)?")]
     public static partial Regex MatchNumber { get; }
     
     /// <summary>
@@ -148,4 +153,110 @@ public static partial class Grammars
             or ']';
     
     // TODO: Parse numbers matched by that horrific MatchNumber regex.
+
+    /// <summary>
+    /// For use with the MatchNumber regex. Checks if match is a hexadecimal number.
+    /// </summary>
+    /// <param name="match">Match to check.</param>
+    /// <returns>True if match is a hexadecimal number, false if not.</returns>
+    public static bool IsHexMatch(Match match) => match.Captures[0].Length != 0;
+
+    /// <summary>
+    /// For use with the MatchNumber regex. Checks if match represents a float or an integer.
+    /// </summary>
+    /// <param name="match"></param>
+    /// <returns></returns>
+    public static bool IsFloatMatch(Match match)
+        => IsHexMatch(match)
+            ? match.Captures[2].Length != 0 || match.Captures[3].Length != 0
+            : match.Captures[5].Length != 0 || match.Captures[6].Length != 0;
+
+    /// <summary>
+    /// For use with the MatchNumber regex. Gets the integer value of the match. 
+    /// </summary>
+    /// <param name="match">Match to parse.</param>
+    /// <returns>Integer value of match. If match has floating-point components, they will be ignored.</returns>
+    public static long ParseIntegerMatch(Match match) 
+        => IsHexMatch(match) 
+            ? ParseHexInteger(match) 
+            : ParseDecimalInteger(match);
+
+    /// <summary>
+    /// For use with the MatchNumber regex. Gets the double-precision floating point value of the match.
+    /// </summary>
+    /// <param name="match">Match to parse.</param>
+    /// <returns>Double-precision floating point of match. If match has no floating point components, it will be the
+    /// integer value of the match in floating point form.</returns>
+    public static double ParseFloatMatch(Match match)
+        => IsHexMatch(match)
+            ? ParseHexFloat(match)
+            : ParseDecimalFloat(match);
+    
+    /// <summary>
+    /// For use with the MatchNumber regex. Parses a decimal integer number from a match.
+    /// </summary>
+    /// <param name="match">Match to parse.</param>
+    /// <returns>64-bit integer value of matched number.</returns>
+    private static long ParseDecimalInteger(Match match)
+        => Convert.ToInt64(match.Captures[4].Value, 10);
+
+    /// <summary>
+    /// For use with the MatchNumber regex. Parses a decimal floating point number from a match.
+    /// </summary>
+    /// <param name="match">Match to parse.</param>
+    /// <returns>Double-precision floating point value of matched number.</returns>
+    private static double ParseDecimalFloat(Match match)
+        => double.Parse(match.Captures[4].Value + match.Captures[5].Value + match.Captures[6].Value);
+
+    /// <summary>
+    /// For use with the MatchNumber regex. Parses a hexadecimal integer number from a match.
+    /// </summary>
+    /// <param name="match">Match to parse.</param>
+    /// <returns>64-bit integer value of matched number.</returns>
+    private static long ParseHexInteger(Match match)
+        => Convert.ToInt64(match.Captures[1].Value, 16);
+
+    /// <summary>
+    /// Lookup dictionary for the value of a hex character as a double.
+    /// </summary>
+    private static readonly Dictionary<char, double> HexLookupDict = new()
+    {
+        { '0', 0.0 }, { '1', 1.0  }, { '2', 2.0  }, { '3', 3.0 }, { '4', 4.0 }, { '5', 5.0 }, { '6', 6.0 }, 
+        { '7', 7.0 }, { '8', 8.0 }, { '9', 9.0 }, { 'a', 10.0 }, { 'b', 11.0 }, { 'c', 12.0 }, { 'd', 13.0 }, 
+        { 'e', 14.0 }, { 'f', 15.0 }, { 'A', 10.0 }, { 'B', 11.0 }, { 'C', 12.0 }, { 'D', 13.0 }, { 'E', 14.0 },
+        { 'F', 15.0 }
+    };
+
+    // TODO: Add verification that number is representable?
+    /// <summary>
+    /// For use with the MatchNumber regex. Parses a hexadecimal floating point number from a match. Does not verify
+    /// that the given number can be represented as a double-precision floating point value.
+    /// </summary>
+    /// <param name="match">Match to parse.</param>
+    /// <returns>Double-precision floating point value of matched number.</returns>
+    // Unfortunately, hexadecimal floats are uncommon enough that there aren't any C# standard library functions for
+    // parsing them, so this has to be bespoke. It is parsed as such:
+    // Significand: Convert to integer as usual.
+    // Fraction: In order 1/16th, 1/256th, 1/4096th, etc. Always representable exactly in a float with enough precision.
+    // Exponent: Power of 2 in decimal form, not hex.
+    // Add the fraction to the significand, then multiply by 2 to the power of the exponent.
+    private static double ParseHexFloat(Match match)
+    {
+        var significand = (double)Convert.ToInt64(match.Captures[1].Value, 16);
+        
+        var fraction = 0.0;
+        var fractionString = match.Captures[2].Value;
+        var placeValue = 1.0 / 16.0;
+        for (var i = 1; i < fractionString.Length; i++)
+        {
+            fraction += HexLookupDict[fractionString[i]] * placeValue;
+            placeValue *= 1.0 / 16.0;
+        }
+
+        var exponent = match.Captures[3].Length != 0
+            ? Convert.ToDouble(match.Captures[3].Value[1..])
+            : 0.0;
+
+        return (significand + fraction) * Math.Pow(2.0, exponent);
+    }
 }
