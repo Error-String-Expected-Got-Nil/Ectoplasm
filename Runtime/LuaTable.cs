2665acb59b947e8146d5c42410da477486cb53cb
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2025-05-20 15:37:49 -0500

    TableImpl abstract

--- Runtime/LuaTable.cs
+++ Runtime/LuaTable.cs
@@ -3,6 +3,26 @@
 public class LuaTable
 {
     // TODO
+
+    /// <summary>
+    /// <para>
+    /// The default length operation for this table, indicated by a unary <c>#</c> operator in Lua.
+    /// </para>
+    /// <para>
+    /// For a table not containing positive integer keys, this will always return 0. Otherwise, it is any positive
+    /// integer key in the table which is a "border", meaning it indexes a non-<see cref="LuaValueKind.Nil"/> value and
+    /// the next positive integer key after it is either not within the table, or is <see cref="LuaValueKind.Nil"/>. In
+    /// particular, this means for a table containing only positive integer keys in a contiguous sequence from 1 to the
+    /// maximum key (as you would get for a table constructed with default indices), this property returns the number of
+    /// elements in the table.
+    /// </para>
+    /// </summary>
+    /// <remarks>
+    /// In Ectoplasm, the highest border is always returned. It is nonetheless not recommended to rely on this fact, as
+    /// it is an implementation detail. Other runtimes may not behave this way, and it may not always be true for
+    /// Ectoplasm.
+    /// </remarks>
+    public long Length => throw new NotImplementedException();
     
     public LuaValue this[LuaValue index]
     {
---
+++ Runtime/Tables/TableImpl.cs
@@ -0,0 +1,43 @@
+﻿namespace Ectoplasm.Runtime.Tables;
+
+/// <summary>
+/// Implementation for a Lua table. May support only a subset of Lua values, in which case it can be upgraded,
+/// causing it to transform into a new implementation which can support the given value.
+/// </summary>
+public abstract class TableImpl
+{
+    /// <summary>
+    /// The default length operator for this table implementation. See <see cref="LuaTable.Length"/> for more details
+    /// as to how this should be computed.
+    /// </summary>
+    public abstract long Length { get; }
+
+    /// <summary>
+    /// Get a value at a particular index.
+    /// </summary>
+    /// <param name="index">The index to check.</param>
+    /// <returns>
+    /// The value contained at the given index if it existed, or <see cref="LuaValueKind.Nil"/> if the index did not
+    /// exist, or was not supported by this implementation.
+    /// </returns>
+    public abstract LuaValue Get(LuaValue index);
+
+    /// <summary>
+    /// Attempt to set the value at a given index to a given value.
+    /// </summary>
+    /// <param name="index">Index to set the value at.</param>
+    /// <param name="value">Value to assign to the given index.</param>
+    /// <returns>
+    /// True if assignment was successful, false if not. Upgrading the implementation should allow assignment. if false
+    /// was returned.
+    /// </returns>
+    public abstract bool Set(LuaValue index, LuaValue value);
+
+    /// <summary>
+    /// Upgrade this table implementation so that it is able to assign the given value to the given index.
+    /// </summary>
+    /// <param name="index">Index to set the value at.</param>
+    /// <param name="value">Value to assign to the given index.</param>
+    /// <returns>The new implementation, which can handle the assignment, with the assignment performed.</returns>
+    public abstract TableImpl Upgrade(LuaValue index, LuaValue value);
+}
