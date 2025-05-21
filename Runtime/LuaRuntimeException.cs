f41c32b93247700758c47a0ab96da7361b14d12a
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2025-05-21 12:48:46 -0500

    TableImpl_Complete

---
+++ Runtime/LuaRuntimeException.cs
@@ -0,0 +1,7 @@
+﻿namespace Ectoplasm.Runtime;
+
+/// <summary>
+/// Represents exceptions caused by Lua-specific errors.
+/// </summary>
+/// <param name="message">Message describing reason for exception.</param>
+public class LuaRuntimeException(string message) : Exception(message);
