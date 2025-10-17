f27cd3c8ae7944fcd0c2f74bbd0ea88db1c5c186
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2025-10-17 09:08:46 -0500

    Tostring now includes hashes
    Hashes should be persistent for any given object over the runtime of a process, and can therefore be used as "addresses" to distinguish reference types when printed, like what native Lua does.

--- Runtime/Stdlib/GlobalFunctions.cs
+++ Runtime/Stdlib/GlobalFunctions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Globalization;
+using System.Text;
 using Ectoplasm.Runtime.Values;
 using Ectoplasm.Utils;
 
@@ -10,10 +11,6 @@ public static class GlobalFunctions
     private static readonly LuaString StringNil = new("nil"u8);
     private static readonly LuaString StringTrue = new("true"u8);
     private static readonly LuaString StringFalse = new("false"u8);
-    private static readonly LuaString StringFunction = new("<function>"u8);
-    private static readonly LuaString StringUserdata = new("<userdata>"u8);
-    private static readonly LuaString StringThread = new("<thread>"u8);
-    private static readonly LuaString StringTable = new("<table>"u8);
     
     public static string LuaToStringUtf16(LuaValue value, bool escapeStrings = false)
         => value.Kind switch
@@ -23,10 +20,10 @@ public static class GlobalFunctions
             LuaValueKind.Integer => value._integer.ToString(),
             LuaValueKind.Float => value._float.ToString(CultureInfo.InvariantCulture),
             LuaValueKind.String => escapeStrings ? value.StringUtf16Safe.GetEscapedString() : value.StringUtf16Safe,
-            LuaValueKind.Function => "<function>",
-            LuaValueKind.Userdata => "<userdata>",
-            LuaValueKind.Thread => "<thread>",
-            LuaValueKind.Table => "<table>",
+            LuaValueKind.Function => $"function 0x{value._ref.GetHashCode():x8}",
+            LuaValueKind.Userdata => $"userdata 0x{value._ref.GetHashCode():x8}",
+            LuaValueKind.Thread => $"thread 0x{value._ref.GetHashCode():x8}",
+            LuaValueKind.Table => $"table 0x{value._ref.GetHashCode():x8}",
             _ => throw new UnreachableException()
         };
 
@@ -42,10 +39,10 @@ public static class GlobalFunctions
             LuaValueKind.Integer => new LuaString(value._integer.ToString()),
             LuaValueKind.Float => new LuaString(value._float.ToString(CultureInfo.InvariantCulture)),
             LuaValueKind.String => (LuaString)value._ref,
-            LuaValueKind.Function => StringFunction,
-            LuaValueKind.Userdata => StringUserdata,
-            LuaValueKind.Thread => StringThread,
-            LuaValueKind.Table => StringTable,
+            LuaValueKind.Function => new LuaString(Encoding.UTF8.GetBytes($"function 0x{value._ref.GetHashCode():x8}")),
+            LuaValueKind.Userdata => new LuaString(Encoding.UTF8.GetBytes($"userdata 0x{value._ref.GetHashCode():x8}")),
+            LuaValueKind.Thread => new LuaString(Encoding.UTF8.GetBytes($"thread 0x{value._ref.GetHashCode():x8}")),
+            LuaValueKind.Table => new LuaString(Encoding.UTF8.GetBytes($"table 0x{value._ref.GetHashCode():x8}")),
             _ => throw new UnreachableException()
         };
 }
