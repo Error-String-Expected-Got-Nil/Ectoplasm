cc048d9309ed4d273f57e1ae8a21bd64e83c39bf
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2025-10-17 08:52:31 -0500

    Remove ChunkDebugInfo from Closure
    Realized it wouldn't actually be accessible there, it'll have to be stored in the LuaState or something

--- Runtime/Functions/Closure.cs
+++ Runtime/Functions/Closure.cs
@@ -4,6 +4,6 @@ namespace Ectoplasm.Runtime.Functions;
 
 /// <summary>
 /// Holds all data necessary for a Lua function closure, intended to be attached as an instance object to Lua function
-/// delegates. Also holds chunk debug info, if captured during compilation.
+/// delegates.
 /// </summary>
-public record Closure(Upvalue[] Upvalues, LuaValue[] Constants, Prototype[] Prototypes, ChunkDebugInfo? DebugInfo);
+public record Closure(Upvalue[] Upvalues, LuaValue[] Constants, Prototype[] Prototypes);
