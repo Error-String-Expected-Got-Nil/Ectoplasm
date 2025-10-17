0496b38b2bd35b3652f92130de80f026dafdf125
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2025-10-16 23:25:51 -0500

    Upvalue class
    Added after doing a good deal of thinking about how Lua function closures should work. Won't be necessary for... awhile... but I figured I may as well add it now.

---
+++ Runtime/Values/Upvalue.cs
@@ -0,0 +1,17 @@
+﻿namespace Ectoplasm.Runtime.Values;
+
+/// <summary>
+/// Basic reference-type container for a <see cref="LuaValue"/>. As implied by the name, this exists primarily for the
+/// implementation of function closure upvalues.
+/// </summary>
+public class Upvalue
+{
+    public LuaValue Value;
+
+    public Upvalue() { }
+
+    public Upvalue(LuaValue value)
+    {
+        Value = value;
+    }
+}
