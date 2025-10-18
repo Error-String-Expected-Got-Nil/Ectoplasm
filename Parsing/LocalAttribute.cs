d8a4939b09a74112b8ed26d4fa454de8da98921a
Error-String-Expected-Got-Nil <103805191+Error-String-Expected-Got-Nil@users.noreply.github.com>
2025-10-18 18:56:44 -0500

    Add note

--- Parsing/LocalAttribute.cs
+++ Parsing/LocalAttribute.cs
@@ -17,7 +17,8 @@ public enum LocalAttribute
     
     /// <summary>
     /// Local has 'close' attribute, indicating it should be closed on falling out of scope. For more details on what
-    /// this means, see the Lua reference manual (version 5.4, section 3.3.8).
+    /// this means, see the Lua reference manual (version 5.4, section 3.3.8). To-be-closed variables are also
+    /// considered constant in the same way as variables with the 'const' attribute.
     /// </summary>
     Close
 }
